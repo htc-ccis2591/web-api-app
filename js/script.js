f6ed59e34f5bbede12e22a3bced26f1411adf165
jllrk1 <jlark002@my.hennepintech.edu>
2016-04-27 20:52:54 -0500

    putting up for test

---
+++ js/script.js
@@ -0,0 +1,84 @@
+$(function () {
+    var $event = $('#events');
+    var $performer = $('#performers');
+    var $venue = $('#venues');
+    var apikey = '';
+    var $mPage = $('#mPage');
+    var $sType = $('#sType');
+    var $displayOptions = $('#displayOptions');
+    $.ajax({
+       beforeSend: function(xhr) {
+           if (xhr.overrideMimeType) {
+               xhr.overrideMimeType("application/json");
+           }
+       } 
+    });
+
+    
+    $mPage.hide();
+    $('form').on('submit', function (e) {
+        apikey = $('#apikey').val();
+        e.preventDefault();
+        $('#API').remove();
+        $mPage.show();
+        //getEventfulData($('#zip').val());
+    });
+        //function to display type of search
+    $event.on("click", function(){
+        var $content = '';
+        var $evSearch = '';
+        $sType.hide();
+        $displayOptions.hide();
+        $mPage.append('<div id = "content"></div>');   
+        $content = $('#content');
+        $content.append('<form id = "eventSearch"><label>What type of event?</label><input type="text" name="eType" id="eType"><input type="submit" name="Submit" value="Submit"></form>');
+        $evSearch = $('#eventSearch');
+        
+        $content.prepend('<h2>Search for types of events</h2>');
+        
+        $evSearch.on('submit', function (e) {
+            eventType = $('#eType').val();
+            e.preventDefault(); 
+            getEventData(eventType);
+        });
+        $('h1').on("click", function(){
+            $sType.show();
+            $displayOptions.show();
+            $content.hide();
+        });
+        
+               //Function to get json data for events
+    function getEventData(eventType) {
+        
+        var eventFullApi = 'http://api.eventful.com/json/events?q=' + eventType;
+        
+        console.log("apikey=" + apikey);
+        console.log("eventType=" + eventType);
+        
+
+        $.getJSON(eventFullApi)
+            .done(function (data) {
+                addNewItem(data);
+            }).fail(function () {
+                $('#API').append('<p> Can not load data </p>');
+            });
+    }
+        
+    
+ 
+
+
+    });
+
+    
+
+   /* function addNewItem(data) {
+        var nItem = '';
+        nItem += '<h3>The weather info for' + ' ' + data.name + '</h3>';
+        nItem += '<p>Current Temperature: ' + convertTemp(data.main.temp) + '</p>';
+        nItem += '<p>' + data.weather[0].description + '</p>';
+        $('#form').append('<div id = weather>' + nItem + '</div>');
+    }*/
+
+
+});
