3e1fea5db13fe8913f09d3f5209b6e83c85eb598
Ryan Freemark <freemark21@gmail.com>
2016-05-09 18:58:07 -0500

    debug

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ var zip = "55344";
     function getPokemon(){
         $.getJSON('//pokeapi.co/api/v2/type/')
         .done(function(data){
-           console.log(data.results[0].name);
+           $('#pokemon .dropdown-menu').append('<li>' + data.results[0].name + '</li>');
         })
         .fail(function(){
             alert("We couldn't catch any Pokemon");
