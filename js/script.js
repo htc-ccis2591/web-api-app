488b9ce8562d4eaa4da871a308964029eff80918
Ryan Freemark <freemark21@gmail.com>
2016-05-10 20:20:57 -0500

    working

--- js/script.js
+++ js/script.js
@@ -17,26 +17,33 @@ $(function(){
         getWeather(zip, weatherKey);
     });
     
-    
-    var wineKey = "c2b17b38e2a85c5f2d0c9fcac6c39cef"
-    function getWine(apiKey){
-        $.getJSON('//services.wine.com/api/beta2/service.svc/JSON/categorymap?filter=categories(490)&apikey=c2b17b38e2a85c5f2d0c9fcac6c39cef')
+   
+    function getStarships(){
+        $.getJSON('//swapi.co/api/starships/')
         .done(function(data){
-            console.log(data);
+            $('#starships').append('<ul>The up to date starships:</ul>');
+            for (var i = 0; i < data.results.length; i++){
+                $('#starships ul').append('<li>' + data.results[i].name + '&nbsp &nbsp<em>Model:</em>&nbsp ' + data.results[i].model + '</li>');
+            };
         })
         .fail(function(){
-            alert('Sorry, something went wrong. No wine for you.');
+            alert('Something went wrong.');
         });
     };
-
-    getWine(wineKey);
+getStarships();
     
-    function getPokemon(){
+    function getPokeType(){
+        
+    }
+
+
+
+   /* function getPokemon(){
         $.getJSON('//pokeapi.co/api/v2/type/')
         .done(function(data){
            for (var i = 0; i < data.results.length; i++){
                $('#pokemon .dropdown-menu').append('<li><a href="' + data.results[i].url +'">' + data.results[i].name + '</a></li>');
-           };
+            };
            $('#pokemon li').on('click', function (e){
                e.preventDefault();
                var url = $(this).attr('href');
@@ -54,17 +61,18 @@ $(function(){
         });
     };
     getPokemon();
-  /*  function loadPokeDmg(){
+    function loadPokeDmg(){
         $('#pokemon li').on('click', function (e){
                e.preventDefault();
                var url = $(this).attr('href');
                $.getJSON(url)
                .done(function (data){
-                   console.log(data);
+                   $('#pokemon .well').append('<p>No damage done to ' + data.damge_relations.no_damage_to + '</p>');
                })
                .fail(function (){
                    alert('Something went wrong');
                });
            });
-    };*/
+    };
+    loadPokeDmg();
 });
